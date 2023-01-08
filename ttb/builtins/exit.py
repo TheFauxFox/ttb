884574875e75c64b99b7c9359f529485227b8707
Faux <37963757+TheFauxFox@users.noreply.github.com>
2023-01-08 00:13:12 -0800

    God I'm lazy. Add shell script to run src properly

--- ttb/builtins/exit.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-aliases = (".exit", "q")
-help = "Exits the program"
-
-
-def run(*args):
-    sys.exit(0)
--- ttb/utils/fancy_print.py
+++
@@ -1,6 +0,0 @@
-from rich.console import Console
-
-console = Console(color_system="truecolor")
-rprint = console.print
-jprint = console.print_json
-cinput = console.input
