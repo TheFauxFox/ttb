db3565b1159b07e32b2fba666f3d1f0e42aaf603
Faux <37963757+TheFauxFox@users.noreply.github.com>
2023-01-08 00:15:53 -0800

    Make an actual package out of this because it's cool

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-from pathlib import Path
-import venv
+from setuptools import setup
 
-venv.create(Path("./.venv"), symlinks=True, with_pip=True)
+setup(
+    name="ttb",
+    version="0.0.1",
+    packages=["src"],
+    entry_points={"console_scripts": ["ttb = src.__main__"]},
+)
